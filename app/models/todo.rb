5792f1a976450f8d9922c4eebe5cb586fa882040
seb1441 <sebastienauriault@gmail.com>
2018-06-25 17:42:44 -0400

    Update models

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,3 +1,5 @@
 class Todo < ApplicationRecord
   belongs_to :user
+
+  as_enum :status, pending: 0, in_progress: 1, done: 2
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,20 @@
 class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :category
+
+  validates :date, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true, length: { minimum: 2 }
+  validates :price, presence: true
+  validates :category_id, presence: true
+
+  validate :transaction_is_unique
+
+  private
+
+    def transaction_is_unique
+      if Transaction.where(date: self.date, user_id: self.user, description: self.description, price: self.price, category: self.category).any?
+        errors.add(:transaction, "already existing transaction")
+      end
+    end
 end
